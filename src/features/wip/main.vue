9bc4eb4950059fcfc71258b752eac590c7b486e1
Fouzder <subrata.fouzder@accenture.com>
2019-12-17 09:22:12 +0530

    chanigng the wip component

--- src/features/wip/main.vue
+++ src/features/wip/main.vue
@@ -1,19 +1,136 @@
-<template lang="pug">
-v-container.my-wip(fluid fill-height text-xs-center)
-  v-layout(justify-center align-center)
-    v-flex.my-wip__hero(text-xs-center)
-      img.my-wip__cone(src='~/@/assets/images/traffic-cone.svg' alt='Work-in-progress')
-      div
-        |Work-in-progress
+<template >
 
+
+  <div class="my-2 text-center" >
+        <v-btn small @click="getResult()">Normal</v-btn>
+    <v-btn
+      class="ma-2"
+      :loading="loading"
+      :disabled="loading"
+      color="secondary"
+      @click="getResult()">
+      Subent Creatin
+      <template v-slot:loader>
+        <span>Getting result...</span>
+      </template>
+    </v-btn>
+   <v-data-table
+    :headers="headers"
+    :items="desserts"
+    class="elevation-1"
+  >
+    <template v-slot:item.calories="{ item }">
+      <v-chip :color="getColor(item.calories)" dark>{{ item.calories }}</v-chip>
+    </template>
+  </v-data-table>
+  </div>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
   name: 'Wip',
 
   data () {
+
+    
     return {
+      loading: false,
+      headers: [
+          {
+            text: 'Dessert (100g serving)',
+            align: 'left',
+            sortable: false,
+            value: 'name',
+          },
+          { text: 'Calories', value: 'calories' },
+          { text: 'Fat (g)', value: 'fat' },
+          { text: 'Carbs (g)', value: 'carbs' },
+          { text: 'Protein (g)', value: 'protein' },
+          { text: 'Iron (%)', value: 'iron' },
+        ],
+        desserts: [
+          {
+            name: 'Frozen Yogurt',
+            calories: 159,
+            fat: 6.0,
+            carbs: 24,
+            protein: 4.0,
+            iron: '1%',
+          },
+          {
+            name: 'Ice cream sandwich',
+            calories: 237,
+            fat: 9.0,
+            carbs: 37,
+            protein: 4.3,
+            iron: '1%',
+          },
+          {
+            name: 'Eclair',
+            calories: 262,
+            fat: 16.0,
+            carbs: 23,
+            protein: 6.0,
+            iron: '7%',
+          },
+          {
+            name: 'Cupcake',
+            calories: 305,
+            fat: 3.7,
+            carbs: 67,
+            protein: 4.3,
+            iron: '8%',
+          },
+          {
+            name: 'Gingerbread',
+            calories: 356,
+            fat: 16.0,
+            carbs: 49,
+            protein: 3.9,
+            iron: '16%',
+          },
+          {
+            name: 'Jelly bean',
+            calories: 375,
+            fat: 0.0,
+            carbs: 94,
+            protein: 0.0,
+            iron: '0%',
+          },
+          {
+            name: 'Lollipop',
+            calories: 392,
+            fat: 0.2,
+            carbs: 98,
+            protein: 0,
+            iron: '2%',
+          },
+          {
+            name: 'Honeycomb',
+            calories: 408,
+            fat: 3.2,
+            carbs: 87,
+            protein: 6.5,
+            iron: '45%',
+          },
+          {
+            name: 'Donut',
+            calories: 452,
+            fat: 25.0,
+            carbs: 51,
+            protein: 4.9,
+            iron: '22%',
+          },
+          {
+            name: 'KitKat',
+            calories: 518,
+            fat: 26.0,
+            carbs: 65,
+            protein: 7,
+            iron: '6%',
+          },
+        ],
     }
   },
 
@@ -21,8 +138,25 @@ export default {
   },
 
   methods: {
+   getResult(){
+    this.loading=true
+     axios.create({
+      baseURL: 'https://2pgl0fwux4.execute-api.us-east-1.amazonaws.com/Stage/hello?user=Root&eventname=CreateSubnet',
+      timeout: 15000
+    }).get().then((data)=> {
+       console.log(data.data.body)
+
+       this.loading=false
+    }
+     )
+
+     
+   
 
   }
+     
+   }
+  
 }
 </script>
 
@@ -34,5 +168,41 @@ export default {
 
   &__cone
     width 100px
+    .custom-loader {
+    animation: loader 1s infinite;
+    display: flex;
+  }
+  @-moz-keyframes loader {
+    from {
+      transform: rotate(0);
+    }
+    to {
+      transform: rotate(360deg);
+    }
+  }
+  @-webkit-keyframes loader {
+    from {
+      transform: rotate(0);
+    }
+    to {
+      transform: rotate(360deg);
+    }
+  }
+  @-o-keyframes loader {
+    from {
+      transform: rotate(0);
+    }
+    to {
+      transform: rotate(360deg);
+    }
+  }
+  @keyframes loader {
+    from {
+      transform: rotate(0);
+    }
+    to {
+      transform: rotate(360deg);
+    }
+  }
 
 </style>
